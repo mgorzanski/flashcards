1b126632ea0cfefb7eb8df086dfd0879d4fb202b
Mateusz <gorzanski.mateusz@gmail.com>
2018-03-13 20:57:14 +0100

    setup mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
 
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => console.log('Welcome!'));
+MongoClient.connect(url, function (err, db) {
+    const database = db.db('flashcards');
+    const collection = database.collection('flashcards');
+    app.get('/', (req, res) => {
+        collection.insertOne({ user: 'test' }, function (err, r) {
+            if (err) {
+                res.send("An error occured");
+            } else {
+                res.send("All well");
+            }
+        });
+    }); 
+});
 
 app.listen(port);
 console.log(`Server started at http://localhost:${port}...`);
